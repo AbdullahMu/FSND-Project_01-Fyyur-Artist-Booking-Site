f93b8ae4b02f4283dcaa16bd7cb7dee731bf973b
AbdullahMu <50756573+AbdullahMu@users.noreply.github.com>
2021-02-25 15:22:07 +0300

    test: run database after completing CRUD

--- app.py
+++ app.py
@@ -5,8 +5,7 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect,
-url_for
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -329,8 +328,8 @@ def delete_venue(venue_id):
     flash(f'Venue {venue_id} was successfully deleted.')
   return render_template('pages/home.html')
 
- '''
- # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
+'''
+  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
   return None
 '''
