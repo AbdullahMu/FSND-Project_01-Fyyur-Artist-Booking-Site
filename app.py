714eb2675a52ddeb07a78252fabff54a7afd4c44
Abdullah AlGhamdi <50756573+AbdullahMu@users.noreply.github.com>
2021-02-25 10:54:40 +0300

    fix: lower capped table names

--- app.py
+++ app.py
@@ -85,11 +85,11 @@ class Artist(db.Model):
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
-  __tablename__= 'Show'
+  __tablename__= 'show'
 
   id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
   start_time = db.Column(db.DateTime, nullable=False)
 
   def __repr__(self):
