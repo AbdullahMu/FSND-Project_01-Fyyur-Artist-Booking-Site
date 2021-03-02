efa00ccce0a41a7abc18e3a08dc1d89137fbc9f4
Abdullah AlGhamdi <50756573+AbdullahMu@users.noreply.github.com>
2021-03-02 14:08:57 +0300

    chore: completed CRUD

--- app.py
+++ app.py
@@ -204,12 +204,13 @@ def show_venue(venue_id):
   # TODO: replace with real venue data from the venues table, using venue_id
 
     # query venues table by input venue_id
-    venue = Venue.query.get(venue_id)
-
+    venue_table_attributes = inspect(Venue).c
+    attribute_list = [attribute.name for attribute in venue_table_attributes]
+    venue = db.session.query(Venue).with_entities(*attribute_list).filter(Venue.id == venue_id).all()
     current_time = datetime.now()
 
     # find past and upcoming shows using input venue_id and current time to filter query on showw table
-    shows_query = db.session.query(Show).join(Artist).with_entities(Show.artist_id, Artist.name, Artist.image_link, Show.start_time).filter(Show.venue_id == venue_id).all()
+    shows_query = session.query(Show).join(Artist).with_entities(Show.artist_id, Artist.name, Artist.image_link, Show.start_time).filter(Show.venue_id == venue_id).all()
 
     past_shows = list()
     upcoming_shows = list()
@@ -229,27 +230,15 @@ def show_venue(venue_id):
         else:
             past_shows.append(shows_data)
 
-    data = dict()
+
+    data = venue[0]._asdict()
 
     # parse returned data dictionary
-    data = {
-       "id": venue.id,
-       "name": venue.name,
-       "genres": venue.genres,
-       "address": venue.address,
-       "city": venue.city,
-       "state": venue.state,
-       "phone": venue.phone,
-       "website": venue.website,
-       "facebook_link": venue.facebook_link,
-       "seeking_talent": venue.seeking_talent,
-       "seeking_description": venue.seeking_description,
-       "image_link": venue.image_link,
-       "past_shows": past_shows,
-       "upcoming_shows": upcoming_shows,
-       "past_shows_count": len(past_shows),
-       "upcoming_shows_count": len(upcoming_shows),
-     }
+    data['past_shows'] = past_shows
+    data['past_shows_count'] = len(past_shows)
+
+    data['upcoming_shows'] = upcoming_shows
+    data['upcoming_shows_count'] = len(upcoming_shows)
 
     return render_template('pages/show_venue.html', venue=data)
 
@@ -377,19 +366,18 @@ def show_artist(artist_id):
 
     # query artist table by input artist_id
     # query venues table by input venue_id
-    artist = session.query(Artist).with_entities(Artist.id, Artist.name, Artist.genres, Artist.city, Artist.state, Artist.phone, Artist.website, Artist.facebook_link, Artist.seeking_venue, Artist.seeking_description, Artist.image_link).filter(Artist.id == artist_id).all()
-    shows_query = session.query(Show).join(Venue).with_entities(Venue.id, Venue.name, Venue.image_link, Show.start_time).filter(Show.artist_id == artist_id).all()
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%S:%M')
+    artist_table_attributes = inspect(Artist).c
+    attribute_list = [attribute.name for attribute in artist_table_attributes]
+
+    artist = db.session.query(Artist).with_entities(*attribute_list).filter(Artist.id == artist_id).all()
+    shows_query = db.session.query(Show).join(Venue).with_entities(Venue.id, Venue.name, Venue.image_link, Show.start_time).filter(Show.artist_id == artist_id).all()
+    current_time = datetime.now()
 
     data = dict()
 
-    artist = artist[0]._asdict()
-    artist_attributes = artist.keys()
+    data = artist[0]._asdict()
 
     shows_query_attributes = shows_query[0]._asdict().keys()
-
-    for attribute in artist_attributes:
-        data[attribute] = artist[attribute]
 
     past_shows = list()
     upcoming_shows = list()
@@ -421,19 +409,12 @@ def show_artist(artist_id):
 def edit_artist(artist_id):
   # populate form with fields from artist with ID <artist_id>
   form = ArtistForm()
-  artist = Artist.query.get(artist_id)
 
-  if artist:
-    form.name.data = artist.name
-    form.city.data = artist.city
-    form.state.data = artist.state
-    form.phone.data = artist.phone
-    form.genres.data = artist.genres
-    form.facebook_link.data = artist.facebook_link
-    form.image_link.data = artist.image_link
-    form.website.data = artist.website
-    form.seeking_venue.data = artist.seeking_venue
-    form.seeking_description.data = artist.seeking_description
+  artist_table_attributes = inspect(Artist).c
+  attribute_list = [attribute.name for attribute in artist_table_attributes]
+
+  artist = db.session.query(Artist).with_entities(*attribute_list).filter(Artist.id == artist_id).all()
+  artist = artist[0]._asdict()
 
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -442,41 +423,33 @@ def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
 
-  # initialize an error handling variable to capture any error and exceptions in the following the try statement
-  error = False
-
   # retrieve artist record from artist table with input artist_id
-  artist = Artist.query.get(artist_id)
-
+  artist_table_attributes = inspect(Artist).c
+  attribute_list = [attribute.name for attribute in artist_table_attributes]
   try:
-    # update existing record attributes with input values from submitted form by assigning it to retrieved ORM object
-    artist.name = request.form['name']
-    artist.city = request.form['city']
-    artist.state = request.form['state']
-    artist.phone = request.form['phone']
-    artist.genres = request.form.getlist('genres')
-    artist.image_link = request.form['image_link']
-    artist.facebook_link = request.form['facebook_link']
-    artist.website = request.form['website']
-    artist.seeking_venue = True if 'seeking_venue' in request.form else False
-    artist.seeking_description = request.form['seeking_description']
 
+      artist = db.session.query(Artist).with_entities(*attribute_list).filter(Artist.id == artist_id).all()
+      artist = artist[0]._asdict()
+      # update existing record attributes with input values from submitted form by assigning it to retrieved ORM object
+      for attribute in artist_table_attributes:
+        if attribute == 'genres':
+            artist[attribute] = request.form.getlist(attribute)
+        elif attribute == 'seeking_talent':
+            artist[attribute] = bool(request.form[attribute])
+        else:
+            artist[attribute] = request.form[attribute]
+
+    db.session.query(Artist).filter(Artist.id == artist_id).update(artist, synchronize_session="fetch")
     # commit ORM object changes to database
     db.session.commit()
-
-  except:
+except Exception as error:
     # rollback pending changes on error
-    error = True
     db.session.rollback()
-    print(sys.exc_info())
+
 
   finally:
     # close current session and parse submission message
     db.session.close()
-  if error:
-    flash('An error occurred. Artist could not be changed.')
-  if not error:
-    flash('Artist was successfully updated!')
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
@@ -484,20 +457,12 @@ def edit_artist_submission(artist_id):
 def edit_venue(venue_id):
   # populate form with fields from venue with ID <venue_id>
   form = VenueForm()
-  venue = Venue.query.get(venue_id)
 
-  if venue:
-    form.name.data = venue.name
-    form.city.data = venue.city
-    form.state.data = venue.state
-    form.phone.data = venue.phone
-    form.address.data = venue.address
-    form.genres.data = venue.genres
-    form.facebook_link.data = venue.facebook_link
-    form.image_link.data = venue.image_link
-    form.website.data = venue.website
-    form.seeking_talent.data = venue.seeking_talent
-    form.seeking_description.data = venue.seeking_description
+  venue_table_attributes = inspect(Venue).c
+  attribute_list = [attribute.name for attribute in venue_table_attributes]
+
+  venue = db.session.query(Venue).with_entities(*attribute_list).filter(Venue.id == venue_id).all()
+  venue = venue[0]._asdict()
 
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
@@ -506,43 +471,33 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+    venue_table_attributes = inspect(Venue).c
+    attribute_list = [attribute.name for attribute in venue_table_attributes]
+    try:
 
-  # initialize an error handling variable to capture any error and exceptions in the following the try statement
-  error = False
+        venue = db.session.query(Venue).with_entities(*attribute_list).filter(Venue.id == venue_id).all()
+        venue = venue[0]._asdict()
+        # update existing record attributes with input values from submitted form by assigning it to retrieved ORM object
+        for attribute in venue_table_attributes:
+          if attribute == 'genres':
+              venue[attribute] = request.form.getlist(attribute)
+          elif attribute == 'seeking_talent':
+              venue[attribute] = bool(request.form[attribute])
+          else:
+              venue[attribute] = request.form[attribute]
 
-  # retrieve venue record from venue table with input venue_id
-  venue = Venue.query.get(venue_id)
+      db.session.query(Venue).filter(Venue.id == venue_id).update(venue, synchronize_session="fetch")
+      # commit ORM object changes to database
+      db.session.commit()
+    except Exception as error:
+      # rollback pending changes on error
+      db.session.rollback()
 
-  try:
-  # update existing record attributes with input values from submitted form by assigning it to retrieved ORM object
-    venue.name = request.form['name']
-    venue.city = request.form['city']
-    venue.state = request.form['state']
-    venue.address = request.form['address']
-    venue.phone = request.form['phone']
-    venue.genres = request.form.getlist('genres')
-    venue.image_link = request.form['image_link']
-    venue.facebook_link = request.form['facebook_link']
-    venue.website = request.form['website']
-    venue.seeking_talent = True if 'seeking_talent' in request.form else False
-    venue.seeking_description = request.form['seeking_description']
 
-    # commit ORM object changes to database
-    db.session.commit()
+    finally:
+      # close current session and parse submission message
+      db.session.close()
 
-  except:
-    # rollback pending changes on error
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-
-  finally:
-    # close curent session and parse venue update submission message
-    db.session.close()
-  if error:
-    flash(f'An error occurred. Venue could not be changed.')
-  if not error:
-    flash(f'Venue was successfully updated!')
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
@@ -559,38 +514,35 @@ def create_artist_submission():
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
 
-  # initialize an error handling variable to capture any error and exceptions in the following the try statement
-  error = False
+    artist_table_attributes = inspect(Artist).c
+    attribute_list = [attribute.name for attribute in artist_table_attributes]
+    try:
 
-  try:
-    name = request.form['name']
-    city = request.form['city']
-    state = request.form['state']
-    phone = request.form['phone']
-    genres = request.form.getlist('genres'),
-    facebook_link = request.form['facebook_link']
-    image_link = request.form['image_link']
-    website = request.form['website']
-    seeking_venue = True if 'seeking_venue' in request.form else False
-    seeking_description = request.form['seeking_description']
+        artist = db.session.query(Artist).with_entities(*attribute_list).filter(Artist.id == artist_id).all()
+        artist = artist[0]._asdict()
+        # update existing record attributes with input values from submitted form by assigning it to retrieved ORM object
+        for attribute in artist_table_attributes:
+          if attribute == 'genres':
+              artist[attribute] = request.form.getlist(attribute)
+          elif attribute == 'seeking_talent':
+              artist[attribute] = bool(request.form[attribute])
+          else:
+              artist[attribute] = request.form[attribute]
 
-    artist = Artist(name=name, city=city, state=state, phone=phone, genres=genres, facebook_link=facebook_link, image_link=image_link, website=website, seeking_venue=seeking_venue, seeking_description=seeking_description)
-    db.session.add(artist)
-    db.session.commit()
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-  # close current session
-    db.session.close()
+      artist = Artist(**artist)
 
-  # on successful db insert, flash success
-  if not error:
-    flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  else:
-  # TODO: on unsuccessful db insert, flash an error instead.
-    flash('An error occurred. Artist ' + request.form['name']+ ' could not be listed.')
+      db.session.add(artist)
+      # commit ORM object changes to database
+      db.session.commit()
+  except Exception as error:
+      # rollback pending changes on error
+      db.session.rollback()
+
+
+    finally:
+      # close current session and parse submission message
+      db.session.close()
+
 
   return render_template('pages/home.html')
 
@@ -605,20 +557,13 @@ def shows():
   #       num_shows should be aggregated based on number of upcoming shows per venue.
 
   # perform left joins on show table with other tables to get info about all shows
-  shows_query = db.session.query(Show).join(Artist).join(Venue).all()
+  shows_query = db.session.query(Show).join(Artist).join(Venue).with_entities(Show.venue_id, Venue.name.label('venue_name'), Show.artist_id, Artist.name.label('artist_name'), Artist.image_link.label('artist_image_link'), Show.start_time).all()
 
   data = []
 
   # loop over retrieved shows info and append data from ORM object to list
   for show in shows_query:
-    data.append({
-      "venue_id": show.venue_id,
-      "venue_name": show.venue.name,
-      "artist_id": show.artist_id,
-      "artist_name": show.artist.name,
-      "artist_image_link": show.artist.image_link,
-      "start_time": show.start_time.strftime('%Y-%m-%d %H:%M:%S')
-    })
+    data.append(show._asdict())
 
   return render_template('pages/shows.html', shows=data)
 
@@ -633,40 +578,28 @@ def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
-  # initialize an error handling variable to capture any error and exceptions in the following the try statement
-  error = False
 
   try:
   # assign submitted values to attributes
-    artist_id = request.form['artist_id']
-    venue_id = request.form['venue_id']
-    start_time = request.form['start_time']
+    form_data = dict()
+    form_data['artist_id'] = request.form['artist_id']
+    form_data['venue_id'] = request.form['venue_id']
+    form_data['start_time'] = request.form['start_time']
 
-    print(request.form)
 
   # parse the entry from submitted values by creating an ORM object then insert it to database
-    show = Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
+    show = Show(**form_data)
     db.session.add(show)
     db.session.commit()
-
-  except:
+    flash('Show was successfully listed!')
+except Exception as error:
   # rollback pending changes on error
-    error = True
     db.session.rollback()
-    print(sys.exc_info())
+    flash('An error occurred. Show could not be listed.')
 
   finally:
   # close curent session
     db.session.close()
-
-  # on successful db insert, flash success
-  if not error:
-      flash('Show was successfully listed!')
-  else:
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-      flash('An error occurred. Show could not be listed.')
 
   return render_template('pages/home.html')
 
